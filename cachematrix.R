17818e4b3537093489459b5cd44159a0b96357fa
Shriharirajeswar <65538278+Shriharirajeswar@users.noreply.github.com>
2020-06-08 18:11:30 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,8 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    ## The matrix to be inverted is written here
   inv<-NULL
   set<-function(y){
     x<<-y
